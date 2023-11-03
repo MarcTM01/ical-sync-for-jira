305df624bdb3a78f09f80915fcdc57e1cde9d7ec
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2023-11-03 18:09:48 +0100

    Integrated prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 /* eslint-env node */
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked', 'prettier'],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     parserOptions: {
